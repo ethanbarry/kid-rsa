cd95d353755cc681d806e47b17a150a2e77d2aee
Ethan Barry <ethanbarry@howdytx.net>
2024-01-24 22:48:48 -0600

    Added driver code.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 fn main() {
-    // Driver code here.
+    let ((n, e), (_, d)) = kid_rsa_keygen(4, 81, 123, 19963);
+    let message = vec![
+        2900509, 4609028, 1549587, 4569295, 1271456, 3854101, 1271456, 3973300, 3854101, 4370630,
+        4092499, 4013033, 4529562, 4410363, 4648761, 4569295, 1271456, 3893834, 4648761, 4569295,
+        4171965, 4370630, 4013033, 4569295, 4569295, 1748252, 1271456, 2781310, 4529562, 4410363,
+        3973300, 4410363, 1748252, 1271456, 4092499, 4410363, 4171965, 4370630, 4092499, 1271456,
+        4410363, 4648761, 4609028, 1271456, 4807693, 4410363, 4648761, 4529562, 1271456, 3973300,
+        4410363, 4410363, 4529562, 1827718,
+    ];
+
+    let plaintext = kid_rsa_decrypt((n, d), message);
+    let text: String = plaintext.into_iter().collect();
+
+    println!("Plaintext: {}", text);
+
+    // Encrypt and print encrypted string.
+    let ciphertext = kid_rsa_encrypt(
+        (n, e),
+        "It's a dangerous business, Frodo, going out your door."
+            .chars()
+            .collect(),
+    );
+
+    println!("\nCiphertext: {:?}", ciphertext);
 }
 
 pub fn kid_rsa_keygen(a: u32, b: u32, a_prime: u32, b_prime: u32) -> ((u32, u32), (u32, u32)) {
